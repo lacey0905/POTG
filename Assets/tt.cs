afa071d1be8c8237beb4c21f4b6d646c10895eb4
DESKTOP-6QHKM6B\LACEY <lacey0905@naver.com>
2017-10-25 14:58:14 +0900

    1025

---
+++ Assets/tt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class tt : NetworkBehaviour {
+
+    public Button tempUI;
+
+    [SyncVar]
+    public float temp = 2f;
+
+    void Start()
+    {
+    }
+
+    void Update() {
+        if (isLocalPlayer) {
+            if (Input.GetKeyDown("t")) {
+                transform.position = new Vector3(temp, 1f, 1f);
+                temp += 2f;
+            }
+        }
+    }
+
+}
